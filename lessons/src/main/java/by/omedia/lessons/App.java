7ea2379f365cd4d4b549e271e886235d8ed7bc62
misterlucky1982 <damonhill1996@tut.by>
2019-06-02 23:43:14 +0300

    initial commit

---
+++ lessons/src/main/java/by/omedia/lessons/App.java
@@ -0,0 +1,13 @@
+package by.omedia.lessons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
